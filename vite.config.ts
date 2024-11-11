be93f1eb023b28307b7736d2fc0b659af377d546
MonicaTohe <Contacto.MonicaHf@gmail.com>
2024-11-11 16:45:10 -0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  
+  base: command === 'build' ? '/aplicacionWebReact/' : '/', // Solo aplicar en producción
   server: {
     host: 'localhost',
     port: 3000,
     open: true,
   },
-  
-})
+}));
